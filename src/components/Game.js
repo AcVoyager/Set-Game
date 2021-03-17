cc1c6833f77b993184fcd3bff0781074091dae27
acvoyager <xcvoyager01@gmail.com>
2021-03-17 00:25:19 -0700

    add pictures; today's work done

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,10 +5,48 @@ function Game(props) {
 
   const difficulty = useSelector(state => state.difficulty);
 
-  return (
-    <div className="container-fluid">
+  const getDiff = (diff) => {
+    if(diff == 1) // use == instead of === on purpose here
+      return "Easy";
+    if(diff == 2)
+      return "Medium";
+    if(diff == 3)
+      return "Hard";
+  }
 
-      {difficulty}
+  return (
+    <div className="container-fluid d-flex flex-grow-1">
+
+      <div className="row flex-grow-1">
+        <div className="col-3 d-flex flex-column justify-content-between flex-grow-1">
+          
+          <div className="mt-5">
+            <h4>{"Difficulty: " + getDiff(difficulty)}</h4>
+          </div>
+          
+          <div className="buttons">
+            <div className="my-3">
+              <button type="button" class="btn btn-primary">Reset</button>
+            </div>
+            <div className="my-3">
+              <button type="button" class="btn btn-success">Draw 3 cards</button>
+            </div>
+          </div>
+
+          <div className="mb-2">
+            <p>Put something here later</p>
+          </div>
+        </div>
+
+        <div className="col-9">
+
+          <div className="game-board">
+            {/* TODO */}
+          </div>
+
+        </div>
+
+      </div>
       
     </div>
   )
